6a2e76b0a83dc25a9c2a0102598307a31a72e757
davsi111 <davsi111@in.ibm.com>
2021-05-09 13:23:50 +0530

    service httpd

---
+++ learning-chef/httpservice.rb
@@ -0,0 +1,14 @@
+service 'httpd' do
+    action :install
+    action :start
+    action :enable
+end
+
+file '/var/www/html/index.html' do
+    content 'its my webeserver'
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+end
+
