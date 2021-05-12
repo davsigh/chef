9bbca65b084d2438cb4090d877dd003416a9c596
root <root@ip-172-31-42-180.ap-south-1.compute.internal>
2021-05-12 09:19:31 +0000

    OHAI practical

--- cookbooks/apache/recipes/webserver.rb
+++ cookbooks/apache/recipes/webserver.rb
@@ -6,8 +6,8 @@ service 'httpd' do
 end
 file '/var/www/html/index.html' do
     content "<h1>New webserver for cookbook testing</h1>
-    <h2>IPADDRESS: #{ ['ipaddress'] } </h2>
-    <h2>HOSTNAME: #{ ['hostname'] } </h2>"
+    <h2>IPADDRESS: #{ node['ipaddress'] } </h2>
+    <h2>HOSTNAME: #{ node['hostname'] } </h2>"
     owner 'root'
     group 'root'
     mode '0755'
