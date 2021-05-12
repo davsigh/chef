78ba0816095b409f544f1c31c0373dfbf7843514
davsi111 <davsi111@in.ibm.com>
2021-05-12 15:45:02 +0530

    template

--- apache/recipes/default.rb
+++
@@ -1,5 +0,0 @@
-#
-# Cookbook:: apache
-# Recipe:: default
-#
-# Copyright:: 2021, The Authors, All Rights Reserved.
--- learning-chef/file.rb
+++
@@ -1,7 +0,0 @@
-file 'hello.txt' do
-    content 'My first file'
-    owner 'root'
-    group 'root'
-    mode '0755'
-    action :create
-end
